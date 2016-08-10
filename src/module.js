3f913e2ec5d0d3c59189ed6d0f569024748ca1e6
aurigadl <aurigadl@gmail.com>
2016-08-10 11:32:30 -0500

    let only the server flask restfull

--- src/module.js
+++
@@ -1,9 +0,0 @@
-// Bootstrapping module
-require("./css/foundation.css");
-import React from 'react'
-import { render  } from 'react-dom'
-import { Router, browserHistory } from 'react-router'
-import routes  from './config/routes'
-
-render(
-    <Router history={browserHistory} routes={routes}/>, document.getElementById('content'));
