c5b347c7f34013c33e9da92b935cc292708d35a5
Cesar Augusto Gonzalez Jimenez <cesar.gonzalezj@agenciadetierras.gov.co>
2019-05-24 11:04:34 -0500

    cambio de pruebas para credenciales del git

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ DEBUG = True
 RBAC_USE_WHITE = True
 TOKEN_SECRET = os.environ.get('SECRET_KEY') or os.urandom(24)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-SQLALCHEMY_ECHO = False
+SQLALCHEMY_ECHO = True 
